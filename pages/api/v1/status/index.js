31a1cb06f09c3a2934f365a21d72898a536d192c
Ismael Sidney <mael-sf@hotmail.com>
2025-12-16 16:54:26 -0300

    test: Testing husky secrets Github Actions

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
+  const apiKey = "AKIAIOSFODNN7EXAMPLE";
+  console.log(apiKey);
   const updatedAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW  server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
